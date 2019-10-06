3b7f684447090d5205d3e967e889db1ee0764003
Konstantin Krivopishin <k.krivopishin@gmail.com>
2019-10-06 18:03:23 +0300

    Module2_Task3_Item1

---
+++ Krivopishin_Module2_Task3_.Item1.ps1
@@ -0,0 +1,6 @@
+#1.	Создайте сценарии *.ps1 для задач из labwork 2, проверьте их работоспостобность. Каждый сценарий должен иметь параметры.
+#1.1.	Сохранить в текстовый файл на диске список запущенных(!) служб. Просмотреть содержимое диска. Вывести содержимое файла в консоль PS.
+param ([string] $File = "f:\Work\EPAM\Labworks\Test.txt")
+Get-Service | Where-Object -Property Status -eq Running | Out-File $File
+Get-ChildItem $File
+Get-Content $File
