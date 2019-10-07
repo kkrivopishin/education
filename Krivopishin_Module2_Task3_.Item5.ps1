6683d4b9a9a0d94e1765470ce1dd137132e5d7f0
Konstantin Krivopishin <k.krivopishin@gmail.com>
2019-10-07 12:19:00 +0300

    Module2_Task3_Item5

--- Krivopishin_Module2_Task3_.Item5.ps1
+++ Krivopishin_Module2_Task3_.Item5.ps1
@@ -2,6 +2,7 @@
 #1.5.1.	Сохранить в CSV-файле информацию обо всех обновлениях безопасности ОС.
 #1.5.2.	Сохранить в XML-файле информацию о записях одной ветви реестра HKLM:\SOFTWARE\Microsoft.
 #1.5.3.	Загрузить данные из полученного в п.1.5.1 или п.1.5.2 файла и вывести в виде списка  разным разными цветами
+[CmdletBinding()]
 param ([parameter(mandatory=$true)] [string]$colorxml,[parameter(mandatory=$true)] [string]$colorcsv)
 Get-HotFix | Export-Csv -Path f:\Work\EPAM\Labworks\hotfix.csv 
 Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft -Recurse| Export-Clixml -Path f:\Work\EPAM\Labworks\reestr.xml
