c929692ffe7b48ef2797e0a43bd506d8b7dc46f2
Konstantin Krivopishin <k.krivopishin@gmail.com>
2019-10-07 12:09:41 +0300

    Module2_Task3_Item3

--- Krivopishin_Module2_Task3_.Item3.ps1
+++ Krivopishin_Module2_Task3_.Item3.ps1
@@ -1,3 +1,4 @@
 #1.3.	Вывести список из 10 процессов занимающих дольше всего процессор. Результат записывать в файл.
+[CmdletBinding()]
 param ([string] $Fproc = "f:\Work\EPAM\Labworks\Processes.txt")
 Get-Process | Sort-Object CPU -Descending | Select-Object -First 10 | Out-File $Fproc
