cb392806287b136285ed6dd9ceab0b0e11a38808
Konstantin Krivopishin <k.krivopishin@gmail.com>
2019-10-20 14:34:14 +0300

    Module3_Task2_1

---
+++ Extra Task 2_1.ps1
@@ -0,0 +1,2 @@
+Invoke-Sqlcmd -ServerInstance "KVM1"-Username sa -Password Kvm092019 -InputFile "K:\MSSQL\DataBases\SQLHumanResourcesDB.sql"
+Invoke-Sqlcmd -ServerInstance "KVM1"-Username sa -Password Kvm092019 -InputFile "K:\MSSQL\DataBases\SQLInternetSalesDB.sql"
