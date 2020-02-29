4c36cdb1e6e94b44f25d6e27556a59ab60e9e798
Kanstantsin Kryvapishyn <Kanstantsin_Kryvapishyn@epam.com>
2020-02-29 17:32:45 +0300

    Final commit

---
+++ Module 3/ApplyTemplates.ps1
@@ -0,0 +1,1 @@
+New-AzResourceGroupDeployment -ResourceGroupName "Training" -TemplateUri "https://raw.githubusercontent.com/kkrivopishin/education/master/Module%203/main.json" -TemplateParameterUri "https://raw.githubusercontent.com/kkrivopishin/education/master/Module%203/parameters.json" -Verbose
