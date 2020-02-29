785f9b4b1e46dfe33cee269c8aceaf0c9e252882
Kanstantsin Kryvapishyn <Kanstantsin_Kryvapishyn@epam.com>
2020-02-29 19:01:15 +0300

    Add creating ResourceGroup

--- Module 3/ApplyTemplates.ps1
+++ Module 3/ApplyTemplates.ps1
@@ -1,1 +1,3 @@
-New-AzResourceGroupDeployment -ResourceGroupName "Training" -TemplateUri "https://raw.githubusercontent.com/kkrivopishin/education/master/Module%203/main.json" -TemplateParameterUri "https://raw.githubusercontent.com/kkrivopishin/education/master/Module%203/parameters.json" -Verbose
+New-AzResourceGroup -Name "Training" -Location "East US"
+
+New-AzResourceGroupDeployment -ResourceGroupName "Training" -TemplateUri "https://raw.githubusercontent.com/kkrivopishin/education/master/Module%203/main.json" -TemplateParameterUri "https://raw.githubusercontent.com/kkrivopishin/education/master/Module%203/parameters.json" -Verbosek
